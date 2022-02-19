c0321097a749145844abb304f24e0e4e09ffbe69
MassiveDynamo <369000923+MassiveDynamo@users.noreply.github.com>
2022-02-19 20:42:00 +0100

    Install .Net 4.8

--- src/FleetCarrierAutopilotApp/Properties/AssemblyInfo.cs
+++ src/FleetCarrierAutopilotApp/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FleetCarrierAutopilotApp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Fleet Carrier Autopilot")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("MassiveDynamo")]
 [assembly: AssemblyProduct("FleetCarrierAutopilotApp")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
@@ -39,17 +39,3 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
-
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
